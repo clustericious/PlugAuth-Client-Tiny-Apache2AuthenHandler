23c708c1746d2de2b1d59401e9edb39e3d0eea6a
Graham Ollis <plicease@cpan.org>
2015-10-20 11:43:55 -0400

    initial implementation

---
+++ README.md
@@ -0,0 +1,34 @@
+# PlugAuth::Client::Tiny::Apache2AuthenHandler
+
+Apache authentication handler for PlugAuth
+
+# SYNOPSIS
+
+In your httpd.conf:
+
+    <Location /protected>
+      PerlAuthenHandler PlugAuth::Client::Tiny::Apache2AuthenHandler
+      AuthType Basic
+      AuthName "My Protected Documents"
+      Require valid-user
+    </Location>
+
+# DESCRIPTION
+
+This module provides an PlugAuth authentication (via [PlugAuth::Tiny](https://metacpan.org/pod/PlugAuth::Tiny)) for
+your legacy Apache2 application.
+
+# SEE ALSO
+
+- [PlugAuth](https://metacpan.org/pod/PlugAuth)
+
+# AUTHOR
+
+Graham Ollis &lt;plicease@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Graham Ollis.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/PlugAuth/Client/Tiny/Apache2AuthenHandler.pm
+++ lib/PlugAuth/Client/Tiny/Apache2AuthenHandler.pm
@@ -2,8 +2,59 @@ package PlugAuth::Client::Tiny::Apache2AuthenHandler;
 
 use strict;
 use warnings;
+use 5.012;
+use PlugAuth::Client::Tiny;
+use Apache2::Access      ();
+use Apache2::RequestUtil ();
+use Apache2::Const       -compile => qw( OK HTTP_UNAUTHORIZED );
 
-# ABSTRACT: Apache authetication handler for PlugAuth
+# ABSTRACT: Apache authentication handler for PlugAuth
 # VERSION
+
+=head1 SYNOPSIS
+
+In your httpd.conf:
+
+ <Location /protected>
+   PerlAuthenHandler PlugAuth::Client::Tiny::Apache2AuthenHandler
+   AuthType Basic
+   AuthName "My Protected Documents"
+   Require valid-user
+ </Location>
+
+=head1 DESCRIPTION
+
+This module provides an PlugAuth authentication (via L<PlugAuth::Tiny>) for
+your legacy Apache2 application.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<PlugAuth>
+
+=back
+
+=cut
+
+sub handler
+{
+  my($r) = @_;
+
+  my($status, $password) = $r->get_basic_auth_pw;
+  return $status unless $status == Apache2::Const::OK;
+
+  my $auth = PlugAuth::Client::Tiny->new($ENV{PLUGAUTH_URL});
+
+  if($auth->auth($r->user, $password))
+  {
+    return Apache2::Const::OK;
+  }
+  else
+  {
+    $r->note_basic_auth_failure;
+    return Apache2::Const::HTTP_UNAUTHORIZED;
+  }
+}
 
 1;
